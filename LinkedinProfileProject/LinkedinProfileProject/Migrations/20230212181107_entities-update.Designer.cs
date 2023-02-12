413f652bb0b2a17035fdc1db2c7ea4002aa509d1
Metehan Biçer <metehanbicer@odakgis.com.tr>
2023-02-12 21:11:53 +0300

    db duzenleme

--- LinkedinProfileProject/LinkedinProfileProject/Migrations/20230212181107_entities-update.Designer.cs
+++ LinkedinProfileProject/LinkedinProfileProject/Migrations/20230212181107_entities-update.Designer.cs
@@ -3,6 +3,7 @@ using LinkedinProfileProject.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LinkedinProfileProject.Migrations
 {
     [DbContext(typeof(LinkedlnProfileContext))]
-    partial class LinkedlnProfileContextModelSnapshot : ModelSnapshot
+    [Migration("20230212181107_entities-update")]
+    partial class entitiesupdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ LinkedinProfileProject/LinkedinProfileProject/Migrations/20230212181107_entities-update.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LinkedinProfileProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class entitiesupdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "Experience");
+
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "Education");
+
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "Education");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "Experience",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                table: "Education",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "Education",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
